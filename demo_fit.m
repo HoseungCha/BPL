c4b7ba735afbb46414015df4b251d96bf288614d
Ho-Seung Cha <hoseungcha@gmail.com>
2020-02-25 17:08:08 +0900

    addpath

--- demo_fit.m
+++ demo_fit.m
@@ -1,10 +1,11 @@
 % Demo of fitting a motor program to an image.
-
+addpath(genpath(pwd));
+% addpath(genpath('D:\OneDrive - 한양대학교\외부프로그램\lightspeed'));
 % Parameters
 K = 5; % number of unique parses we want to collect
 verbose = true; % describe progress and visualize parse?
 include_mcmc = false; % run mcmc to estimate local variability?
-fast_mode = true; % skip the slow step of fitting strokes to details of the ink?
+fast_mode = false; % skip the slow step of fitting strokes to details of the ink?
 
 if fast_mode
     fprintf(1,'Fast mode skips the slow step of fitting strokes to details of the ink.\n');
